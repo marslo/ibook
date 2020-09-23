cb42a9ec465399f8b4aac46a7bb88a288ca77f81
marslo <marslo@marvell.com>
2020-09-23 23:19:17 +0800

    using _book/* instead of markdown

--- docs/jenkins/script/list/plugins/cli-plugin.groovy
+++
@@ -1,4 +0,0 @@
-// https://support.cloudbees.com/hc/en-us/articles/216351528-How-Do-I-Analyse-Plugins-and-Dependencies-for-an-Instance
-
-def plugins = jenkins.model.Jenkins.instance.getPluginManager().getPlugins()
-plugins.each {println "${it.getShortName()}: ${it.getVersion()}"}
