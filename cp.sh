b5a8b29db9cf75d19bdd32553ff304ab351fd558
marslo <marslo.jiao@gmail.com>
2021-07-06 21:10:02 +0800

    update cp.sh for no changes situation

--- cp.sh
+++ cp.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-startMsg=$(git log -1 --pretty=format:'%s')
-startRev=$(git log marslo --pretty="%h - %s" | grep "${startMsg}" | awk '{print $1}')
-reversions=$(git rev-list marslo ${startRev}..origin/marslo | tac | xargs)
+branch='marslo'
 
-git cherry-pick $reversions
+git fetch --all --force
+git fetch --force origin "${branch}":remotes/origin/"${branch}"
+git fetch --prune --prune-tags --force origin
+
+startMsg=$(git log -1 --pretty=format:'%s')
+startRev=$(git log origin/"${branch}" --pretty="%h - %s" | grep "${startMsg}" | awk '{print $1}')
+reversions=$(git rev-list origin/"${branch}" ${startRev}..origin/"${branch}" | tac | xargs)
+
+if [ -z "${reversions}" ]; then
+  echo "no commits between master and ${branch} branch. Exit."
+else
+  git cherry-pick ${reversions}
+  git push -u --force origin master
+fi
