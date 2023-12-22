d0ac6f7c651a4683e8b6d5158358563a696b23ed
marslo <marslo.jiao@gmail.com>
2023-12-22 00:10:23 -0800

    update deploy for toc via fd instead of find

--- cp.sh
+++ cp.sh
@@ -8,11 +8,13 @@ git fetch --prune --prune-tags --force origin
 
 startMsg=$(git log -1 --pretty=format:'%s')
 startRev=$(git log origin/"${branch}" --pretty="%h - %s" | grep "${startMsg}" | awk '{print $1}')
-reversions=$(git rev-list origin/"${branch}" ${startRev}..origin/"${branch}" | tac | xargs)
+reversions=$(git rev-list origin/"${branch}" "${startRev}"..origin/"${branch}" | tac | xargs)
 
 if [ -z "${reversions}" ]; then
   echo "no commits between master and ${branch} branch. Exit."
 else
-  git cherry-pick ${reversions}
+  git cherry-pick "${reversions}"
   git push -u --force origin master
 fi
+
+# vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh
