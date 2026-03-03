14aad06d7db37654a0032eb34bb7077a8ff03462
marslo <marslo.jiao@gmail.com>
2026-03-02 20:51:36 -0800

    refactor(fzf,awesomeshell): refactor the fzf and add the smart open
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- linux/util/progressBar2.sh
+++ linux/util/progressBar2.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # totalLines=$(wc -l $file | awk '{print $1}')  # num. lines in file
 totalLines=100
 barLen=30
@@ -7,9 +9,9 @@ FILL='------------------------------'
 count=0
 while [ ${count} -lt ${totalLines} ]; do
   # update progress bar
-  count=$(( ${count}+ 1 ))
-  percent=$(( (${count} * 100 / ${totalLines} * 100)/ 100 ))
-  i=$(( ${percent} * ${barLen} / 100 ))
+  count=$(( count + 1 ))
+  percent=$(( ( count * 100 * 100 / totalLines ) / 100 ))
+  i=$(( percent * barLen / 100 ))
   echo -ne "\r[${BAR:0:$i}${FILL:$i:barLen}] ${count}/${totalLines} (${percent}%)"
   sleep .1
 done
