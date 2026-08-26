44c6bbfe540c99811f0009822b5f611c6ce69b33
marslo <marslo.jiao@gmail.com>
2026-08-26 14:20:46 -0700

    build(deploy.sh): update deploy.sh
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# shellcheck source=/dev/null disable=SC1078,SC1079,SC2015,SC2216
+# shellcheck source=/dev/null disable=SC2155
 # =============================================================================
 #   FileName : deploy.sh
-#     Author : marslo.jiao@gmail.com
+#     Author : marslo
 #    Created : 2020-09-27 22:03:34
-# LastChange : 2026-02-17 17:31:47
+# LastChange : 2026-08-26 14:20:32
 # =============================================================================
 
 # @credit: https://github.com/ppo/bash-colors
@@ -19,17 +19,17 @@ modules="${root}/node_modules"
 book="${root}/_book"
 branch='gh-pages'
 # declare remotes=$(git remote -v | sed -n -re 's:^origin\W*(\S+)\W*\(push\)$:\1:gp')
-remotes=$(git remote get-url origin)
-msg=$(git --no-pager show HEAD --no-patch --format="%s")
-usage="""NAME
-  $(c B)deploy.sh - to quickly deploy _book/* into gh-pages branch $(c)
+declare remotes=$(git remote get-url origin)
+declare msg=$(git --no-pager show HEAD --no-patch --format="%s")
+usage="NAME
+  $(c 0B)deploy.sh - to quickly deploy _book/* into gh-pages branch $(c)
 
 USAGE
-  $(c sG)$ $0 [help] [function name]$(c)
+  $(c 0Gs)$ $0 [help] [function name]$(c)
 
 NOTICE
-  add command $(c Y)'built'$(c) in $(c Csi)package.json$(c) as below:
-  $(c Gi)\`\`\`json
+  add command $(c Y)'built'$(c) in $(c 0Csi)package.json$(c) as below:
+  $(c 0Gi)\`\`\`json
   {
     \"scripts\": {
       \"built\": \"gitbook install && gitbook build\",
@@ -39,27 +39,27 @@ NOTICE
   more details can be found by $(c Y)$ $0 info$(c)
 
 EXAMPLE
-  $(c Wdi)# deploy _book into remote repository gh-pages branch$(c)
-  $(c Y)$ $0 doDeploy$(c)
+  $(c 0Wdi)# deploy _book into remote repository gh-pages branch$(c)
+  $(c 0Y)$ $0 doDeploy$(c)
 
-  $(c Wdi)# show current information$(c)
-  $(c Y)$ $0 info$(c)
+  $(c 0Wdi)# show current information$(c)
+  $(c 0Y)$ $0 info$(c)
 
 INDEPENDENT FUNCTION NAME
-"""
+"
 
-info="""
-  $(c M)BASIC INFO :$(c)
-             $(c Wdi)[TEMP DIR]$(c) : $(c Ci)${target}$(c)
-    $(c Wdi)[REMOTE REPOSITORY]$(c) : $(c Ci)${remotes}$(c)
-     $(c Wdi)[BRANCH TO DEPLOY]$(c) : $(c Ci)${branch}$(c)
-       $(c Wdi)[COMMIT MESSAGE]$(c) : $(c Ci)${msg}$(c)
+info="
+  $(c 0M)BASIC INFO :$(c)
+               $(c 0Wdi)TEMP DIR$(c) : $(c 0Ci)${target}$(c)
+      $(c 0Wdi)REMOTE REPOSITORY$(c) : $(c 0Ci)${remotes}$(c)
+       $(c 0Wdi)BRANCH TO DEPLOY$(c) : $(c 0Ci)${branch}$(c)
+         $(c 0Wdi)COMMIT MESSAGE$(c) : $(c 0Ci)${msg}$(c)
 
-  $(c M)NPM COMMANDS :$(c)
-       $(c Wdi)$ npm run clean$(c)  : $(c Yi)$(grep \"clean\"  "${root}"/package.json  | sed -n -re 's/.*:\W*"([^"]+)".*$/\1/p')$(c)
-       $(c Wdi)$ npm run built$(c)  : $(c Yi)$(grep \"built\"  "${root}"/package.json  | sed -n -re 's/.*:\W*"([^"]+)".*$/\1/p')$(c)
-       $(c Wdi)$ npm run deploy$(c) : $(c Yi)$(grep \"deploy\" "${root}"/package.json  | sed -n -re 's/.*:\W*"([^"]+)".*$/\1/p')$(c)
-"""
+  $(c 0M)NPM COMMANDS :$(c)
+       $(c 0Wdi)$ npm run clean$(c)  : $(c 0Yi)$(grep \"clean\"  "${root}"/package.json  | sed -n -re 's/.*:\W*"([^"]+)".*$/\1/p')$(c)
+       $(c 0Wdi)$ npm run built$(c)  : $(c 0Yi)$(grep \"built\"  "${root}"/package.json  | sed -n -re 's/.*:\W*"([^"]+)".*$/\1/p')$(c)
+       $(c 0Wdi)$ npm run deploy$(c) : $(c 0Yi)$(grep \"deploy\" "${root}"/package.json  | sed -n -re 's/.*:\W*"([^"]+)".*$/\1/p')$(c)
+"
 
 function help() {
   echo -e "${usage}"
@@ -67,36 +67,33 @@ function help() {
   declare -F -p | sed -re "s:^.*-f(.*)$:\t\1:g"
 }
 
-function info() {
-  echo -e "${info}"
-}
+function info() { echo -e "${info}"; }
 
 function build() {
-  [ -d ./node_modules ] && rm -rf ./node_modules
-  [ -d ./_book ] && rm -rf ./_book
+  test -d ./node_modules && rm -rf ./node_modules
+  test -d ./_book && rm -rf ./_book
   npm run built
 }
 
 function installModules() {
-  [[ -d "${modules}" ]] || gitbook install
+  test -d "${modules}" || gitbook install
 }
 
 # to rebuilt for changed file only
 function rebuiltToc() {
   # xargs doctoc --github --notitle --update-only --maxlevel 3 >/dev/null \
   #        < <( fd . "$(git rev-parse --show-toplevel)"/docs --type f --extension md --exclude SUMMARY.md --exclude README.md )
-  xargs doctoc --github --notitle --update-only --maxlevel 3 >/dev/null \
-        < <(git diff --name-only --diff-filter=AMCT 'HEAD..HEAD^')
+  xargs doctoc --github --notitle --update-only --maxlevel 3 >/dev/null < <(git diff --name-only --diff-filter=AMCT 'HEAD..HEAD^')
 }
 
 function rePush(){
   git add --all "$(git rev-parse --show-toplevel)"
   git commit --signoff --amend --no-edit --allow-empty
-  git push -u --force origin "$(git rev-parse --abbrev-ref HEAD)"
+  git push -u --force origin HEAD:"$(git rev-parse --abbrev-ref HEAD)"
 }
 
 function updateRepo() {
-  if [ """$(git rev-parse remotes/origin/"${branch}")""" != """$(git -C "${target}" rev-parse HEAD)""" ]; then
+  if [[ """$(git rev-parse remotes/origin/"${branch}")""" != """$(git -C "${target}" rev-parse HEAD)""" ]]; then
     git -C "${target}" fetch origin --force "${branch}"
     # git -C "${target}" rebase -v refs/remotes/origin/${branch}
     git -C "${target}" reset --hard refs/remotes/origin/${branch}
@@ -116,12 +113,12 @@ function updateBook() {
   cd "$(git rev-parse --show-toplevel)" || return
 
   if ! npm run built; then
-    echo "ERROR: FAILED on gitbook build. Exiting."
+    echo -e "$(c 0Rs)ERROR$(c): $(c 0Wi)FAILED on gitbook build. Exiting.$(c)"
     popd || return
     exit 1
   else
-    yes | rm -rf "${target:?}"/*
-    yes | cp -Rf "${book}"/* "${target}"/
+    command rm -rf "${target:?}"/*
+    command cp -Rf "${book}"/* "${target}"/
 
     cd "${target}" || exit
 
@@ -134,16 +131,21 @@ function updateBook() {
       pre-commit run --all-files
     }
     git add --all .
-    targetMsg=$(git show remotes/origin/gh-pages --no-patch --format="%s")
-    signed="$(git log -n1 --format='%(trailers:key=Signed-off-by,valueonly,separator=%x2C)' | command grep -q "$(git config user.email)"; echo $?)";
-    [ 0 -ne "${signed}" ] && sopt='--signoff' || sopt='';
-    if [ "${targetMsg}" = "${msg}" ]; then
-      echo '~~> force push without create new commit: '
-      eval "git commit ${sopt} --amend --no-edit --allow-empty"
+
+    targetMsg=$(git --no-pager show remotes/origin/gh-pages --no-color --no-patch --format="%s")
+    local -a cmd=(git commit)
+    git log -1 --format='%(trailers:key=Signed-off-by,valueonly,separator=%x2C)' |
+        command grep -q "$(git config user.email)" || cmd+=('--signoff')
+
+    if [[ "${targetMsg}" = "${msg}" ]]; then
+      echo -e "$(c 0Ci)~~> force push without create new commit:$(c)"
+      cmd+=(--amend --no-edit --allow-empty)
     else
-      eval "git commit ${sopt} -am '${msg}'"
+      cmd+=(-am "${msg}")
     fi
-    git push origin gh-pages --force
+
+    "${cmd[@]}"
+    git push -u origin HEAD:refs/heads/gh-pages --force
 
     popd || return
   fi
